59999d0738a746f7f85ed567e7d73bdd34c42821
kkrrissttofferrssonn <christopheralduncan@gmail.com>
2022-10-30 14:45:54 -0400

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,20 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+
+// A "ref" is a reactive data source that stores a value.
+// Technically, we don't need to wrap the string with ref()
+// in order to display it, but we will see in the next
+// example why it is needed if we ever intend to change
+// the value.
+const message = ref('PaperLibrary');
 </script>
 
 <template>
   <div>
-    <h1>J-DU</h1>
+    <h1 class="paperlibrary">J-DU</h1>
+    <h1>{{ message }}</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -18,6 +27,9 @@ import HelloWorld from './components/HelloWorld.vue';
 </template>
 
 <style scoped>
+.paperlibrary {
+  background-color: Green;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
