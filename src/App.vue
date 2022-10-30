e68551263b2a78e82c1209c4b3e4f6ffcb1ba958
kkrrissttofferrssonn <christopheralduncan@gmail.com>
2022-10-30 15:01:27 -0400

    1 file modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,36 @@
 import { ref } from 'vue';
 import TreeItem from './TreeItem.vue';
 import HelloWorld from './components/HelloWorld.vue';
+import PolyGraph from './PolyGraph.vue'
+import { ref, reactive } from 'vue'
 
+const newLabel = ref('')
+const stats = reactive([
+  { label: 'A', value: 100 },
+  { label: 'B', value: 100 },
+  { label: 'C', value: 100 },
+  { label: 'D', value: 100 },
+  { label: 'E', value: 100 },
+  { label: 'F', value: 100 }
+])
+
+function add(e) {
+  e.preventDefault()
+  if (!newLabel.value) return
+  stats.push({
+    label: newLabel.value,
+    value: 100
+  })
+  newLabel.value = ''
+}
+
+function remove(stat) {
+  if (stats.length > 3) {
+    stats.splice(stats.indexOf(stat), 1)
+  } else {
+    alert("Can't delete more!")
+  }
+}
 const treeData = ref({
   name: 'My Tree',
   children: [
